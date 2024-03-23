39f722323244446e5d76f30cd1ab5b8004a69f2f
Camiinthecity <camille.rouet@pm.me>
2024-03-23 22:33:01 +0100

    Ajout de comptage et coefficient multiplication

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,9 +1,8 @@
 extends Node
 
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Player.level = 1
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,16 +4,34 @@ signal hitByRessource(ressource)
 
 @export var speed = 400 # in pix/sec
 
-var playerEvolution = "BIG" # TINY, LITTLE, BIG (= F0, F1, F2)
+var playerEvolution # TINY, LITTLE, BIG (= F0, F1, F2)
 var playerMoveState = "idle" # eat, eat_poison, idle, walk_down, walk_lateral, walk_up
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
+var resource_counter_0 = 0
+var resource_counter_1 = 0
+var resource_counter_2 = 0
+var resource_counter_3 = 0
+var resource_counter_4 = 0
+var resource_counter_5 = 0
+
+var level = 1
+
+ # the balance bar
+var coefficientOfVariationResource = 0
+var pointMultiplier = 1 #
+
+ # which percentage to which the balance bar reach a step
+const CVresourceStep1 = 0.2
+const CVresourceStep2 = 0.1
+
 # called at start
 func _ready():
 	$AnimatedSprite2D.hide()
-	setPlayerEvolution("BIG")
+	setPlayerEvolution("LITTLE")
 	setPlayerMoveState("idle")
 	$AnimatedSprite2D.show()
 
@@ -81,8 +99,49 @@ func setPlayerEvolution(playerEvolution_):
 
 func _on_hit_by_ressource(ressource):
 	
-	var time = Time.get_time_dict_from_system()
-	$AnimatedSprite2D.set_modulate(Color(randf(),randf(),randf()))
-	print(ressource, " entered in Player")
-	print(time) # {day:X, dst:False, hour:xx, minute:xx, month:xx, second:xx, weekday:x, year:xxxx}
-	pass # Replace with function body.
+	# Hit only if the resource is in OK state
+	if(ressource.state == "OK"):
+		var time = Time.get_time_dict_from_system()
+		print(ressource, " entered in Player")
+		print(time) # {day:X, dst:False, hour:xx, minute:xx, month:xx, second:xx, weekday:x, year:xxxx}
+		$AnimatedSprite2D.set_modulate(Color(randf(),randf(),randf()))
+		
+		$AudioStreamPlayer.play()
+		
+		ressource.state = "TOUCHED"
+		match ressource.slimeType:
+			ressource.SlimeTypeEnum.BLUE_LEVEL_1:
+				resource_counter_0 += 1
+			ressource.SlimeTypeEnum.PINK_LEVEL_1:
+				resource_counter_1 += 1
+			ressource.SlimeTypeEnum.BLUE_H1_LEVEL_2:
+				resource_counter_2 += 1
+			ressource.SlimeTypeEnum.BLUE_H2_LEVEL_2:
+				resource_counter_3 += 1
+			ressource.SlimeTypeEnum.PINK_E1_LEVEL_2:
+				resource_counter_4 += 1
+			ressource.SlimeTypeEnum.PINK_E2_LEVEL_2:
+				resource_counter_5 += 1
+		
+		var meanResource = 1
+		var varianceResource = 0
+		if(level == 1):
+			meanResource = resource_counter_0 + resource_counter_1
+			varianceResource = 1/2 * ((resource_counter_0 - meanResource)**2 + (resource_counter_1 - meanResource)**2)
+		elif(level == 2):
+			meanResource = resource_counter_2 + resource_counter_3 + resource_counter_4 + resource_counter_5
+			varianceResource = 1/4 * ((resource_counter_2 - meanResource)**2 + (resource_counter_3 - meanResource)**2 + (resource_counter_4 - meanResource)**2 + (resource_counter_5 - meanResource)**2)
+			
+		var standDeviationResource = sqrt(varianceResource)
+		coefficientOfVariationResource = standDeviationResource / meanResource
+		
+		if(coefficientOfVariationResource < CVresourceStep1):
+			pointMultiplier = 3
+		elif(coefficientOfVariationResource < CVresourceStep2):
+			pointMultiplier = 5
+		else:
+			pointMultiplier = 1
+	print("resource_counter_0 ", resource_counter_0)
+	print("resource_counter_1 ", resource_counter_1)
+	print("coefficientOfVariationResource ", pointMultiplier)
+	print("pointMultiplier ", pointMultiplier)
