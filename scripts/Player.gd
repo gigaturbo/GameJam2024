200e8db0657dd7cf571fb98f8159d646a6bb1ba4
Camiinthecity <camille.rouet@pm.me>
2024-03-23 10:35:01 +0100

    Player Sprite + peut bouger

---
+++ scripts/Player.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+
+@export var speed = 400 # in pix/sec
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+# base methods, commented by CR
+#func _physics_process(delta):
+#	# Add the gravity.
+#	if not is_on_floor():
+#		velocity.y += gravity * delta
+#
+#	# Handle Jump.
+#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+#		velocity.y = JUMP_VELOCITY
+#
+#	# Get the input direction and handle the movement/deceleration.
+#	# As good practice, you should replace UI actions with custom gameplay actions.
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
+#
+#	move_and_slide()
+
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	# Movement
+	get_input()
+	move_and_slide()
+	
+	# Orientation
+	if Input.get_vector("left", "right", "up", "down").x != 0:
+		$AnimatedSprite2D.set_flip_h(Input.get_vector("left", "right", "up", "down").x < 0)
+	
