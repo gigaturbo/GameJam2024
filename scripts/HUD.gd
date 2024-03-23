69795b89e0e59096dd68e98d2962e1ee1f8b8f69
gigaturbo <gigaturbo@protonmail.com>
2024-03-23 12:18:05 +0100

    HUD: add timer

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -2,27 +2,49 @@ extends Control
 
 var blueBar
 var redBar
+var label
+var timerRemaining
+var isPaused = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	blueBar = $"MarginContainer/HSplitContainer/Color Stats Container/Color Progress Bars/VSplitContainer/BlueProgress"
 	redBar = $"MarginContainer/HSplitContainer/Color Stats Container/Color Progress Bars/VSplitContainer/RedProgress"
+	label = $MarginContainer2/LabelTimer
 	init()
 	
-func init():
-	blueBar.value = 0
-	redBar.value = 0
+
+func init(timerValue= 3*60):
+	isPaused = true
+	setColorStats(0, 0)
+	setTimerSeconds(timerValue)
+
+func setPaused(paused):
+	isPaused = paused
+
+	# Set the remaining timer label from `timerRemaining`
+func setTimerLabel():
+	label.text = str("Timer", "\n", int(timerRemaining/60),"'", int(timerRemaining - int(timerRemaining/60)*60), "''")
+
+func setTimerSeconds(s):
+	timerRemaining = s
+	setTimerLabel()
+
+func removeTimerSeconds(s):
+	setTimerSeconds(timerRemaining - s)	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	addColorStat(delta * 1, delta * 2)
+	pass
+	#if not isPaused:
+	#	addColorStat(delta * 1, delta * 2)
+	#	removeTimerSeconds(delta)
 
-
+# Sets the color bar values
 func setColorStats(blue, red):
 	blueBar.value = clampf(blue, 0, 100)
 	redBar.value = clampf(red, 0, 100)
 
+# Add a value to the color bars 
 func addColorStat(blue, red):
 	setColorStats(blueBar.value + blue, redBar.value + red)
-	
-	
 	
