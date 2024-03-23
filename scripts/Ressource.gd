17351e0912e91a90f6280f16c74263443fd842f6
Camiinthecity <camille.rouet@pm.me>
2024-03-23 13:15:41 +0100

    Reglage du HUD dans main

--- scripts/Ressource.gd
+++ scripts/Ressource.gd
@@ -44,10 +44,10 @@ func randomImpulseMove(anImpulseSpeed):
 	apply_impulse(impulse_vec)
 
 
-func _on_body_entered(body):
+
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
 	
 	var time = Time.get_time_dict_from_system()
+	print(body, " entered")
 	print(time) # {day:X, dst:False, hour:xx, minute:xx, month:xx, second:xx, weekday:x, year:xxxx}
-	$AnimatedSprite2D.scale = $AnimatedSprite2D.scale * 0.5
-	pass # Replace with function body.
-
+	$AnimatedSprite2D.set_modulate(Color(randf(),randf(),randf()))
