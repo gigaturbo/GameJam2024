6b18356ec1eed657b4ec3f0249b3948f2655c832
Camiinthecity <camille.rouet@pm.me>
2024-03-23 11:38:34 +0100

    Basic Player and Ressource interaction

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,32 +6,14 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-# base methods, commented by CR
-#func _physics_process(delta):
-#	# Add the gravity.
-#	if not is_on_floor():
-#		velocity.y += gravity * delta
-#
-#	# Handle Jump.
-#	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-#		velocity.y = JUMP_VELOCITY
-#
-#	# Get the input direction and handle the movement/deceleration.
-#	# As good practice, you should replace UI actions with custom gameplay actions.
-#	var direction = Input.get_axis("ui_left", "ui_right")
-#	if direction:
-#		velocity.x = direction * SPEED
-#	else:
-#		velocity.x = move_toward(velocity.x, 0, SPEED)
-#
-#	move_and_slide()
 
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = input_direction.normalized()
 	velocity = input_direction * speed
 
 func _physics_process(delta):
--- scripts/Ressource.gd
+++ scripts/Ressource.gd
@@ -1,32 +1,53 @@
 extends RigidBody2D
 
+@export var impulseSpeed = 50 # impulse speed in pix/sec
+@export var maxSpeed = 100 # in pix/sec
 
 var velocity = Vector2.ZERO
 var speed = 0
 var spawned = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.hide()
+	set_lock_rotation_enabled(true)
+	set_contact_monitor(true)
+	max_contacts_reported = 3
+
+
 	start()
 
 func start():
 	spawned = true
-	changeBehavior()
 	$AnimatedSprite2D.show()
 	$TimerChangeDirection.start()
-	
-
-func changeBehavior():
-	velocity = Vector2.from_angle(randf() * 2* PI).normalized()
-	speed = randi_range(5,10)
+	randomImpulseMove(impulseSpeed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if spawned:
-		position = position + velocity * speed * delta
-
+	pass
 
 func _on_timer_change_direction_timeout():
-	changeBehavior()
+	randomImpulseMove(impulseSpeed)
 	
+	if linear_velocity.length() > maxSpeed:
+		linear_velocity = linear_velocity.normalized() * maxSpeed
+
+func randomImpulseMove(anImpulseSpeed):
+	# stop me
+	linear_velocity = Vector2(0, 0)
+
+	# give me an impulse
+	var x_impulse = 0.5 - randf() # from -0.5 to 0.5, no unit
+	var y_impulse = 0.5 - randf() # from -0.5 to 0.5, no unit
+	var impulse_vec = Vector2(x_impulse, y_impulse).normalized() * anImpulseSpeed
+	apply_impulse(impulse_vec)
+
+
+func _on_body_entered(body):
 	
+	var time = Time.get_time_dict_from_system()
+	print(time) # {day:X, dst:False, hour:xx, minute:xx, month:xx, second:xx, weekday:x, year:xxxx}
+	$AnimatedSprite2D.scale = $AnimatedSprite2D.scale * 0.5
+	pass # Replace with function body.
+
