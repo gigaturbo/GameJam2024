e952575ef8475d14b97eca9e1a6ed743e48c44ea
gigaturbo <gigaturbo@protonmail.com>
2024-03-23 20:09:45 +0100

    Resource: 0-5

--- scripts/Ressource.gd
+++ scripts/Ressource.gd
@@ -1,12 +1,16 @@
 extends Area2D
 
+enum SlimeTypeEnum {BLUE_LEVEL_1, PINK_LEVEL_1, BLUE_H1_LEVEL_2, BLUE_H2_LEVEL_2, PINK_E1_LEVEL_2, PINK_E2_LEVEL_2}
+enum RandomSlimeLevel2Enum {OFF, RANDOM_BLUE, RANDOM_PINK, RANDOM_VARIANT1, RANDOM_VARIANT2}
+
 @export var impulseSpeed = 50 # impulse speed in pix/sec
 @export var maxSpeed = 100 # in pix/sec
 @export var brakingSpeed = 10 # in pix/sec/sec
-@export var slimeColor : int  = 0 # Two colors of slimes : 0 to 1
-@export var slimeHat : int = 0 # Two hats of slimes : 0 to 2
-@export var randomizeSlimeColor : bool = false
-@export var randomizeSlimeHat : bool = false
+
+@export_group("SLIME TYPE SETTINGS |!| Random level_2 prio. sur level_1 |!|")
+@export var slimeType : SlimeTypeEnum
+@export var randomizeSlimeColorLevel1 : bool = false
+@export var randomizeSlimeLevel2 : RandomSlimeLevel2Enum = RandomSlimeLevel2Enum.OFF
 
 var state = "OK" # OK TOUCHED DEAD
 var velocity = Vector2.ZERO
@@ -20,51 +24,71 @@ func _ready():
 #	set_lock_rotation_enabled(true)
 #	set_contact_monitor(true)
 #	max_contacts_reported = 3
+	$AnimatedSprite2D.position = Vector2(300,300)
 	start()
 
 
 func getAnimationName(action):
 	var anim = ""
-	match slimeColor:
-		0:
-			anim = str(anim, "A")
-		1:
-			anim = str(anim, "B")
-	match slimeHat:
-		0:
+	# TODO: set collisions boxes for 1-5
+	$CollisionShape2D_0.disabled = false
+	$CollisionShape2D_1.disabled = true
+	$CollisionShape2D_2.disabled = true
+	$CollisionShape2D_0.show()
+	$CollisionShape2D_1.hide()
+	$CollisionShape2D_2.hide()
+	match slimeType:
+		SlimeTypeEnum.BLUE_LEVEL_1:
 			anim = str(anim, "0")
-			$CollisionShape2D_0.disabled = false
-			$CollisionShape2D_1.disabled = true
-			$CollisionShape2D_2.disabled = true
-			$CollisionShape2D_0.show()
-			$CollisionShape2D_1.hide()
-			$CollisionShape2D_2.hide()
-		1:
+		SlimeTypeEnum.PINK_LEVEL_1:
 			anim = str(anim, "1")
-			$CollisionShape2D_0.disabled = true
-			$CollisionShape2D_1.disabled = false
-			$CollisionShape2D_2.disabled = true
-			$CollisionShape2D_0.hide()
-			$CollisionShape2D_1.show()
-			$CollisionShape2D_2.hide()
-		2:
+		SlimeTypeEnum.BLUE_H1_LEVEL_2:
 			anim = str(anim, "2")
-			$CollisionShape2D_0.disabled = true
-			$CollisionShape2D_1.disabled = true
-			$CollisionShape2D_2.disabled = false
-			$CollisionShape2D_0.hide()
-			$CollisionShape2D_1.hide()
-			$CollisionShape2D_2.show()
-			
+		SlimeTypeEnum.BLUE_H2_LEVEL_2:
+			anim = str(anim, "3")
+		SlimeTypeEnum.PINK_E1_LEVEL_2:
+			anim = str(anim, "4")
+		SlimeTypeEnum.PINK_E2_LEVEL_2:
+			anim = str(anim, "5")
+	
 	return str(anim, "_", action)
 	
-
+	
 func start():
 	spawned = true
-	if randomizeSlimeColor:
-		slimeColor = randi_range(0,1)
-	if randomizeSlimeHat:
-		slimeHat = randi_range(0,2)
+	if randomizeSlimeColorLevel1: # Simple random choice
+		match randi_range(0,1):
+			0:
+				slimeType = SlimeTypeEnum.BLUE_LEVEL_1
+			1:
+				slimeType = SlimeTypeEnum.PINK_LEVEL_1
+
+	if randomizeSlimeLevel2 != RandomSlimeLevel2Enum.OFF:
+		match randomizeSlimeLevel2:
+			RandomSlimeLevel2Enum.RANDOM_BLUE:
+				match randi_range(0,1):
+					0:
+						slimeType = SlimeTypeEnum.BLUE_H1_LEVEL_2
+					1:
+						slimeType = SlimeTypeEnum.BLUE_H2_LEVEL_2
+			RandomSlimeLevel2Enum.RANDOM_PINK:
+				match randi_range(0,1):
+					0:
+						slimeType = SlimeTypeEnum.PINK_E1_LEVEL_2
+					1:
+						slimeType = SlimeTypeEnum.PINK_E2_LEVEL_2
+			RandomSlimeLevel2Enum.RANDOM_VARIANT1:
+				match randi_range(0,1):
+					0:
+						slimeType = SlimeTypeEnum.BLUE_H1_LEVEL_2
+					1:
+						slimeType = SlimeTypeEnum.PINK_E1_LEVEL_2
+			RandomSlimeLevel2Enum.RANDOM_VARIANT2:
+				match randi_range(0,1):
+					0:
+						slimeType = SlimeTypeEnum.BLUE_H2_LEVEL_2
+					1:
+						slimeType = SlimeTypeEnum.PINK_E2_LEVEL_2
 
 	$AnimatedSprite2D.animation = getAnimationName("idle")
 	$AnimatedSprite2D.show()
