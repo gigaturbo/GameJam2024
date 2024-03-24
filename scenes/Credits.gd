652f6d0442db93551631378559c39ba1d417a3ff
DAMIE Marc <marc.damie@inria.fr>
2024-03-24 07:44:10 +0100

    Implémentation du menu principal

---
+++ scenes/Credits.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("esc"):
+		get_tree().change_scene_to_file("res://scenes/HomeMenu.tscn")
+
+
+func _on_return_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/HomeMenu.tscn")
---
+++ scripts/HomeMenu.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_launch_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/Credits.tscn")
