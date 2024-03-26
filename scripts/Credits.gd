62d2f65e59f9335ac0880b3f1a41acb0be92a6c2
Camiinthecity <camille.rouet@pm.me>
2024-03-26 11:52:21 +0100

    balance musique

--- scripts/Credits.gd
+++ scripts/Credits.gd
@@ -10,6 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("esc"):
+		$AudioStreamPlayer.stop()
 		get_tree().change_scene_to_file("res://scenes/HomeMenu.tscn")
 
 
